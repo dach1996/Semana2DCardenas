cc1173d6729b1b7ecb1a0f00eea3057fce6764d6
dach1996 <dach19962012@gmail.com>
2020-10-24 09:33:57 -0500

    Agregar archivos de proyecto.

---
+++ Semana2DCardenas/Semana2DCardenas/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Semana2DCardenas
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var notaSeguimiento1 = Convert.ToDouble(entryNotaSeguimiento1.Text) * 0.3;
+                var notaExamen1 = Convert.ToDouble(entryExamen1.Text) * 0.2;
+                var notaParcial1 = notaSeguimiento1 + notaExamen1;
+                labelNotaParcial1.Text = "Nota Parcial: " + notaParcial1.ToString();
+
+                var notaSeguimiento2 = Convert.ToDouble(entryNotaSeguimiento2.Text) * 0.3;
+                var notaExamen2 = Convert.ToDouble(entryExamen2.Text) * 0.2;
+                var notaParcial2 = (notaSeguimiento2 + notaExamen2);
+                labelNotaParcial2.Text = "Nota Parcial: " + notaParcial2.ToString();
+                
+                var notaFinal = notaParcial1 + notaParcial2;
+                textnotaFinal.Text = "NOTA FINAL: " + notaFinal;
+                if (notaFinal >= 7)
+                {
+                    textEstado.Text = "APROBADO";
+                }
+                else if(notaFinal <7)
+                {
+                    textEstado.Text = "REPROBADO";
+                }
+            }
+            catch(Exception ex)
+            {
+                DisplayAlert("Error", ex.Message, "OK");
+            }  
+        }
+    }
+}
---
+++ Semana2DCardenas/Semana2DCardenas/login.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Semana2DCardenas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class login : ContentPage
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAbrirVentanaDos_Clicked(object sender, EventArgs e)
+        {
+            if (txtUser.Text == "admin" && textPass.Text == "12345")
+            {
+                await Navigation.PushAsync(new MainPage());
+            }
+            else
+            {
+                DisplayAlert("Error", "Acceso no Autorizado", "ok");
+            }
+        }
+    }
+}
